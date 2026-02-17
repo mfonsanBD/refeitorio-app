b673b02d37adaa26cb02d5a30ce66dfd4ad929ae
Mike Santos <iddeiasimplesmike@gmail.com>
2026-02-17 15:59:41 -0300

    Corrige dados privados que estavam públicos

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://refeitorio-api-production.up.railway.app/',
+  baseURL: `${process.env.EXPO_PUBLIC_API_URL}/`,
 });
 
 export default {
